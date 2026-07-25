ce55effa63c4034cc7cd95ec6f1a288a769ecb8f
Cukull <didosyukur123@gmail.com>
2026-07-25 12:51:19 +0700

    style: add highly realistic tablet bezel with camera hole

--- filmku-next/src/components/ui/container-scroll-animation.tsx
+++ filmku-next/src/components/ui/container-scroll-animation.tsx
@@ -84,11 +84,18 @@ export const Card = ({
         rotateX: rotate,
         scale,
         boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
+          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003, inset 0 0 0 1px rgba(255,255,255,0.2)",
       }}
-      className="max-w-5xl -mt-16 mx-auto h-[22rem] md:h-[32rem] w-full border-4 border-[#6C6C6C] p-2 md:p-6 bg-[#222222] rounded-[30px] shadow-2xl relative z-20"
+      className="max-w-5xl -mt-16 mx-auto h-[24rem] md:h-[40rem] w-full border-[4px] border-[#b3b3b3] p-4 md:p-10 bg-[#000000] rounded-[40px] shadow-2xl relative z-20"
     >
-      <div className="h-full w-full overflow-hidden rounded-2xl bg-gray-100 dark:bg-zinc-900 md:rounded-2xl md:p-4 relative z-10">
+      {/* Tablet Camera Hole */}
+      <div className="absolute top-2 md:top-4 left-1/2 -translate-x-1/2 flex items-center justify-center space-x-2">
+        <div className="w-2 h-2 md:w-3 md:h-3 rounded-full bg-[#111] shadow-[inset_0_0_2px_rgba(0,0,0,1)] border border-[#333]"></div>
+        <div className="w-1 h-1 rounded-full bg-[#111] opacity-50"></div>
+      </div>
+
+      {/* The actual screen area */}
+      <div className="h-full w-full overflow-hidden rounded-[16px] md:rounded-[24px] bg-[#0a0a0a] relative z-10 border border-[#222]">
         {children}
       </div>
     </motion.div>
